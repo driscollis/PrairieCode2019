b1c1852579a39c7109c0a357ab849fd197f1864e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-17 17:13:43 +0800

    Delete code directory

--- code/hello.py
+++
@@ -1,6 +0,0 @@
-from reportlab.pdfgen import canvas
-
-c = canvas.Canvas("hello.pdf")
-# Coords in points (72 points in an inch)
-c.drawString(100, 750, "Welcome to Reportlab!")
-c.save()
--- code/pdf_merging2.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-merger = PyPDF2.PdfFileMerger()
-
-paths = ['rotate_pages.pdf', 'watermark.pdf']
-for path in paths:
-    merger.append(open(path, 'rb'))
-
-merger.write(open("merged.pdf", 'wb'))
